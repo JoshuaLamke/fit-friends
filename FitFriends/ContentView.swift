95af3c6e04501552d8673b0b02800e3db6262135
Matthew O'Meara <matthewreiomeara@gmail.com>
2021-03-16 22:04:06 -0400

    Add background and logo.

--- FitFriends/ContentView.swift
+++ FitFriends/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            LinearGradient(gradient: .init(colors: [Color("Color"),Color("Color2"),Color("Color3")]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            loginPage()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct loginPage : View {
+    @State var index = 0;
+    var body :some View{
+        VStack {
+            
+            Image("Logo")
+                .resizable()
+                .frame(width: 200, height:180)
+            
+            HStack{
+                Button(action: {
+                    
+                }) {
+                    Text("New")
+                        .foregroundColor(self.index == 0 ? .black : .white)
+                        .fontWeight(.bold)
+                        .padding(.vertical,10)
+                        .frame(width: (UIScreen.main.bounds.width - 50)/2)
+                    
+                }.background(self.index == 1 ? Color.white: Color.clear)
+                .clipShape(Capsule())
+            }.background(Color.black.opacity(0.1))
+            .clipShape(Capsule())
+        }
     }
 }
