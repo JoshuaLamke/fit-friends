524891699d0fe527e0060d4371774d4e3398f739
Matthew O'Meara <matthewreiomeara@gmail.com>
2021-03-16 22:51:40 -0400

    Add email address, password, login.

--- FitFriends/ContentView.swift
+++ FitFriends/ContentView.swift
@@ -59,6 +59,75 @@ struct loginPage : View {
                 .clipShape(Capsule())
             }.background(Color.black.opacity(0.1))
             .clipShape(Capsule())
+            .padding(.top, 25)
+            
+            Login()
+        }
+        .padding()
+    }
+}
+
+struct Login : View {
+    
+    @State var mail = ""
+    @State var pass = ""
+    
+    var body : some View{
+        VStack{
+            
+            VStack{
+                
+                HStack(spacing: 15){
+                    
+                    Image(systemName: "envelope")
+                        .foregroundColor(.black)
+                    
+                    TextField("Enter Email Address", text: self.$mail)
+                    
+                }.padding(.vertical, 20)
+                
+                Divider()
+                
+                HStack(spacing: 15){
+                    
+                    Image(systemName: "lock")
+                        .resizable()
+                        .frame(width:15, height:18)
+                        .foregroundColor(.black)
+                    
+                    SecureField("Enter Password", text: self.$pass)
+                    
+                    Button(action: {
+                    }) {
+                        Image(systemName: "eye")
+                            .foregroundColor(.black)
+                    }
+                    
+                }.padding(.vertical, 20)
+            }
+            //add padding around username and password
+            .padding(.vertical)
+            .padding(.horizontal,20)
+            .padding(.bottom,40)
+            .background(Color.white)
+            .cornerRadius(10)
+            .padding(.top,25)
+            
+            Button(action: {
+                
+            }){
+                Text("LOGIN")
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+                    .padding(.vertical)
+                    .frame(width: UIScreen.main.bounds.width - 100)
+                
+            }.background (LinearGradient(gradient: .init(colors: [Color("Color2"),Color("Color1"),Color("Color")]), startPoint: .leading, endPoint: .trailing))
+//            }.background (LinearGradient(gradient: .init(colors: [Color("Color2")]), startPoint: .leading, endPoint: .trailing))
+            .cornerRadius(8)
+            .offset(y: -40)
+            .padding(.bottom,-40)
+            .shadow(radius: 25)
         }
     }
 }
